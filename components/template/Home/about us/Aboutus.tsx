11adb537318ab0cb44aacc3211ba34ef3a58fbb1
abdelrahman mohamed <a.elmetwally10@gmail.com>
2024-01-22 01:53:04 +0200

    updated new tasks dont

--- components/template/Home/about us/Aboutus.tsx
+++ components/template/Home/about us/Aboutus.tsx
@@ -8,75 +8,78 @@ import React from 'react';
 
 const Aboutus = () => {
   return (
-    <div className='container my-[35px] '>
-      <div className='flex flex-col items-center'>
-        <h3 className='text-[29px] lg:text-[34px] text-primary font-[TajawalBold,sans-serif]'>
-          ماذا عننا؟
-        </h3>
-        <p className='text-main text-[15px] lg:text-[17px] text-center'>
-          تعليم القرءان الكريم بالأحكام والقراءه والكتابة بطريقة نور البيان
-        </p>
-      </div>
-
-      {/* Grid Card */}
-      <div className='flex flex-col sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-12 md:gap-20 mt-[40px]'>
+    <div className='my-[35px] about-me-section'>
+      <div className='container my-16'>
         <div className='flex flex-col items-center'>
-          <AboutusIcon1 />
-          <h4 className='text-[18px] lg:text-[22px] text-secondary mt-[10px] font-[TajawalMedium,sans-serif]'>
-            تحفيظ القرءان
-          </h4>
-          <p className='text-textGray text-[15px] lg:text-[16px] xs:w-[60%] sm:!w-[90%] text-center font-[TajawalMedium,sans-serif]'>
-            تحفيظ القرآن ، وشرح المعاني
+          <h3 className='text-[29px] lg:text-[34px] text-[#EFBB43] font-[TajawalBold,sans-serif]'>
+            ماذا عننا؟
+          </h3>
+          <p className='text-white text-[15px] lg:text-[17px] text-center'>
+            تعليم القرءان الكريم بالأحكام والقراءه والكتابة بطريقة نور البيان
           </p>
         </div>
 
-        <div className='flex flex-col items-center'>
-          <AboutusIcon2 />
-          <h4 className='text-[18px] lg:text-[22px] text-secondary mt-[10px] font-[TajawalMedium,sans-serif]'>
-            إختار وقتك!
-          </h4>
-          <p className='text-textGray text-[15px] lg:text-[16px] xs:w-[60%] sm:!w-[90%] text-center font-[TajawalMedium,sans-serif]'>
-            إختيار الوقت المناسب للأم مع معلمات متخصصات وبرضو تفسير وشرح للآيات
-          </p>
-        </div>
-        <div className='flex flex-col items-center'>
-          <AboutusIcon3 />
-          <h4 className='text-[18px] lg:text-[22px] text-secondary mt-[10px] font-[TajawalMedium,sans-serif]'>
-            معلم معك أول ب أول
-          </h4>
-          <p className='text-textGray text-[15px] lg:text-[16px] xs:w-[60%] sm:!w-[90%] text-center font-[TajawalMedium,sans-serif]'>
-            الأهتمام بشكل فردي لكل طفل مع معلم خاص بيه عشان نضمن الاستفادة وعدم
-            التشتت
-          </p>
-        </div>
-        <div className='flex flex-col items-center'>
-          <AboutusIcon4 />
-          <h4 className='text-[18px] lg:text-[22px] text-secondary mt-[10px] font-[TajawalMedium,sans-serif]'>
-            عروض خاصه للأمهات
-          </h4>
-          <p className='text-textGray text-[15px] lg:text-[16px] xs:w-[60%] sm:!w-[90%] text-center font-[TajawalMedium,sans-serif]'>
-            منسناش الأمهات اللي عاوزين يحفظوا القرآن ووقتهم وانشغالتهم مش
-            مساعدينهم
-          </p>
-        </div>
-        <div className='flex flex-col items-center'>
-          <AboutusIcon5 />
-          <h4 className='text-[18px] lg:text-[22px] text-secondary mt-[10px] font-[TajawalMedium,sans-serif]'>
-            جوائز مميزه
-          </h4>
-          <p className='text-textGray text-[15px] lg:text-[16px] xs:w-[60%] sm:!w-[90%] text-center font-[TajawalMedium,sans-serif]'>
-            التفسير وطبعا تطبيق الكلام ده على واقع الأطفال
-          </p>
-        </div>
-        <div className='flex flex-col items-center'>
-          <AboutusIcon6 />
-          <h4 className='text-[18px] lg:text-[22px] text-secondary mt-[10px] font-[TajawalMedium,sans-serif]'>
-            الدور التربوي
-          </h4>
-          <p className='text-textGray text-[15px] lg:text-[16px] xs:w-[60%] sm:!w-[90%] text-center font-[TajawalMedium,sans-serif]'>
-            وتشجيعهم على التحلي بالاخلاق اللي ذكرها القرآن يعني دور تربوي مش بس
-            تعليمي
-          </p>
+        {/* Grid Card */}
+        <div className='flex flex-col sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-12 md:gap-20 mt-[40px]'>
+          <div className='flex flex-col items-center'>
+            <AboutusIcon1 />
+            <h4 className='text-[18px] lg:text-[22px] text-secondary mt-[10px] font-[TajawalMedium,sans-serif]'>
+              تحفيظ القرءان
+            </h4>
+            <p className='text-white text-[15px] lg:text-[16px] xs:w-[60%] sm:!w-[90%] text-center font-[TajawalMedium,sans-serif]'>
+              تحفيظ القرآن ، وشرح المعاني
+            </p>
+          </div>
+
+          <div className='flex flex-col items-center'>
+            <AboutusIcon2 />
+            <h4 className='text-[18px] lg:text-[22px] text-secondary mt-[10px] font-[TajawalMedium,sans-serif]'>
+              إختار وقتك!
+            </h4>
+            <p className='text-white text-[15px] lg:text-[16px] xs:w-[60%] sm:!w-[90%] text-center font-[TajawalMedium,sans-serif]'>
+              إختيار الوقت المناسب للأم مع معلمات متخصصات وبرضو تفسير وشرح
+              للآيات
+            </p>
+          </div>
+          <div className='flex flex-col items-center'>
+            <AboutusIcon3 />
+            <h4 className='text-[18px] lg:text-[22px] text-secondary mt-[10px] font-[TajawalMedium,sans-serif]'>
+              معلم معك أول ب أول
+            </h4>
+            <p className='text-white text-[15px] lg:text-[16px] xs:w-[60%] sm:!w-[90%] text-center font-[TajawalMedium,sans-serif]'>
+              الأهتمام بشكل فردي لكل طفل مع معلم خاص بيه عشان نضمن الاستفادة
+              وعدم التشتت
+            </p>
+          </div>
+          <div className='flex flex-col items-center'>
+            <AboutusIcon4 />
+            <h4 className='text-[18px] lg:text-[22px] text-secondary mt-[10px] font-[TajawalMedium,sans-serif]'>
+              عروض خاصه للأمهات
+            </h4>
+            <p className='text-white text-[15px] lg:text-[16px] xs:w-[60%] sm:!w-[90%] text-center font-[TajawalMedium,sans-serif]'>
+              منسناش الأمهات اللي عاوزين يحفظوا القرآن ووقتهم وانشغالتهم مش
+              مساعدينهم
+            </p>
+          </div>
+          <div className='flex flex-col items-center'>
+            <AboutusIcon5 />
+            <h4 className='text-[18px] lg:text-[22px] text-secondary mt-[10px] font-[TajawalMedium,sans-serif]'>
+              جوائز مميزه
+            </h4>
+            <p className='text-white text-[15px] lg:text-[16px] xs:w-[60%] sm:!w-[90%] text-center font-[TajawalMedium,sans-serif]'>
+              التفسير وطبعا تطبيق الكلام ده على واقع الأطفال
+            </p>
+          </div>
+          <div className='flex flex-col items-center'>
+            <AboutusIcon6 />
+            <h4 className='text-[18px] lg:text-[22px] text-secondary mt-[10px] font-[TajawalMedium,sans-serif]'>
+              الدور التربوي
+            </h4>
+            <p className='text-white text-[15px] lg:text-[16px] xs:w-[60%] sm:!w-[90%] text-center font-[TajawalMedium,sans-serif]'>
+              وتشجيعهم على التحلي بالاخلاق اللي ذكرها القرآن يعني دور تربوي مش
+              بس تعليمي
+            </p>
+          </div>
         </div>
       </div>
     </div>
