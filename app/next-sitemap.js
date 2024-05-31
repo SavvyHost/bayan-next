6dd544c3c2fcb7b5bc2690dc8a57a50154d55438
abdulRahman elshikh <abdulrahmanelshikh2001@gmail.com>
2024-06-01 00:53:22 +0300

    handel site map

---
+++ app/next-sitemap.js
@@ -0,0 +1,35 @@
+const fetch = require('node-fetch');
+
+const getDynamicPaths = async () => {
+  // استبدل هذا برابط API الخاص بك أو منطق جلب البيانات الديناميكية
+  const res = await fetch('https://backend.bayan-academy.com/');
+  const data = await res.json();
+  
+  return data.map(item => `/dynamic/${item.slug}`);
+};
+
+module.exports = async () => {
+  const dynamicPaths = await getDynamicPaths();
+
+  return {
+    siteUrl: "https://backend.bayan-academy.com/",
+    generateRobotsTxt: true,
+    transform: async (config, path) => {
+      return {
+        loc: "https://backend.bayan-academy.com/",
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        changefreq: 'daily',
+        priority: 0.7,
+      };
+    },
+    additionalPaths: async (config) => {
+      const dynamicPaths = await getDynamicPaths();
+      return dynamicPaths.map((path) => {
+        return {
+          loc: "https://backend.bayan-academy.com/", 
+          lastmod: new Date().toISOString(),
+        };
+      });
+    },
+  };
+};
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,12 @@ module.exports = {
     siteUrl:  'https://backend.bayan-academy.com/',
     // generateRobotsTxt: true, // (optional)
     // ...other options
+    transform: async (config, path) => {
+      return {
+        loc: path, // The location of the URL
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        changefreq: 'daily',
+        priority: 0.7,
+      };
+    },
   }
