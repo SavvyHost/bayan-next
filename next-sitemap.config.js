4c297fec112a4112a3cac11fd47e9d5fa8869341
abdulRahman elshikh <abdulrahmanelshikh2001@gmail.com>
2024-06-01 00:56:01 +0300

    handel site map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl:  'https://backend.bayan-academy.com/',
+    siteUrl:  'https://www.bayan-academy.com/',
     // generateRobotsTxt: true, // (optional)
     // ...other options
     transform: async (config, path) => {
